1b6fd95b5e6cebc02c37997891a82d18ef910899
natam369 <58681656+natam369@users.noreply.github.com>
2020-02-25 12:30:56 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,15 @@
 variable "key_name" {
-  default = "wwe"
+  default = "capstone"
 }
 
 variable "pvt_key" {
-  default = "/root/.ssh/coe.pem"
+  default = "/root/.ssh/bikee.pem"
 }
 
 variable "us-east-zones" {
-  default = ["us-west-1a", "us-west-1b"]
+  default = ["us-west-2a", "us-west-2b"]
 }
 
 variable "sg-id" {
-  default = "sg-0e6cb204a0842565d"
+  default = "sg-0283912e679692daa"
 }
